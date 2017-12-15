e19de615d9fbca2955bd9bb899ac4b2c88d56e79
LR <lisa.yankovskaya@gmail.com>
2017-12-15 19:33:46 +0200

    wp-decoder

---
+++ scripts/wordpieces_decoder.sh
@@ -0,0 +1,5 @@
+spm_decode --model=../data/eten_wp.model --input_format=piece < ../data/lab3_wp_beam5.en > ../data/lab3_wp_beam5_dewp.en
+spm_decode --model=../data/eten_wp.model --input_format=piece < ../data/lab3_wp_beam8.en > ../data/lab3_wp_beam8_dewp.en
+spm_decode --model=../data/eten_wp.model --input_format=piece < ../data/lab3_wp_beam10.en > ../data/lab3_wp_beam10_dewp.en
+spm_decode --model=../data/eten_wp.model --input_format=piece < ../data/lab3_wp_beam12.en > ../data/lab3_wp_beam12_dewp.en
+
